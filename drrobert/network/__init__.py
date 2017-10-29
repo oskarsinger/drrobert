08492f433665be6e31ea76592cb3b31940da83dc
oskar <oskarsinger@gmail.com>
2017-10-29 17:48:38 -0400

    Added existing drrobert code

---
+++ drrobert/network/__init__.py
@@ -0,0 +1,5 @@
+from .generate import *
+from .misc import *
+
+from . import subset
+from . import spectral
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='drrobert',
+    version='0.01',
+    packages=[
+        'drrobert',
+        'drrobert.data_structures',
+        'drrobert.network',
+        'drrobert.network.subset'])
