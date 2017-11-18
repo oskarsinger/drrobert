c3114e287a794be262e608ad02032fe64cf1d991
oskar <oskarsinger@gmail.com>
2017-11-18 15:02:51 -0500

    Fixed a bug

--- drrobert/stats/eval.py
+++ drrobert/stats/eval.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_binary_classification_eval(self, y, y_hat):
+def get_binary_classification_eval(y, y_hat):
 
     get_fc = lambda x: float(np.count_nonzero(x))
     p = get_fc(y > 0)
@@ -8,12 +8,12 @@ def get_binary_classification_eval(self, y, y_hat):
     n = get_fc(y < 0)
     n_hat = get_fc(y_hat < 0)
     tp = get_fc(np.logical_and(y_hat == y, y_hat > 0))
-    tn = get_fc(np.logical_and(y_hat == y, y_hat < o))
+    tn = get_fc(np.logical_and(y_hat == y, y_hat < 0))
     accuracy = (tp + tn) / y.shape[0]
     sensitivity = tp / p
     specificity = tn / n
     precision = tp / p_hat
-    f1 = 2 * precision * recall / (precision + recall)
+    f1 = 2 * precision * sensitivity / (precision + sensitivity)
 
     return {
         'accuracy': accuracy,
