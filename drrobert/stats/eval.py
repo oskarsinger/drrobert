77e74fe1b77d927f4d39bdc87bc49099441eb845
Oskar Singer <oskarsinger@gmail.com>
2018-01-12 17:32:02 -0500

    Added auroc to eval

--- drrobert/stats/eval.py
+++ drrobert/stats/eval.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from sklearn.metrics import roc_auc_score
+
 def get_binary_classification_eval(y, y_hat):
 
     get_fc = lambda x: float(np.count_nonzero(x))
@@ -15,10 +17,12 @@ def get_binary_classification_eval(y, y_hat):
     precision = 1 if p_hat == 0 else tp / p_hat
     f1 = 0 if precision + sensitivity == 0 else \
         2 * precision * sensitivity / (precision + sensitivity)
+    auroc = roc_auc(y, y_hat)
 
     return {
         'accuracy': accuracy,
         'sensitivity': sensitivity,
         'specificity': specificity,
         'precision': precision,
-        'f1': f1}
+        'f1': f1,
+        'auroc': auroc}
