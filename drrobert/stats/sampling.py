5d21107ec9854425c37f66003a3be99dde0d071c
Oskar Singer <oskarsinger@gmail.com>
2018-01-28 17:38:03 -0500

    Optional parameter

--- drrobert/stats/sampling.py
+++ drrobert/stats/sampling.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-def log_uniform(low, high):
+def log_uniform(low, high, factor=None):
 
     power = np.random.uniform(low=low, high=high)
-    factor = np.random.uniform(high=10)
+
+    if factor is None:
+        factor = np.random.uniform(high=10)
 
     return factor * 10**power
