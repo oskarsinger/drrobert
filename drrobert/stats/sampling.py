0fcd6dca06881d4b84004f25ecbe9e578492a49c
Oskar Singer <oskarsinger@gmail.com>
2018-01-29 15:26:47 -0500

    Fixed range of factor

--- drrobert/stats/sampling.py
+++ drrobert/stats/sampling.py
@@ -5,6 +5,6 @@ def log_uniform(low, high, factor=None):
     power = np.random.uniform(low=low, high=high)
 
     if factor is None:
-        factor = np.random.uniform(high=10)
+        factor = np.random.uniform(low=1, high=10)
 
     return factor * 10**power
