7d5acaa8e47d04fafa5bc93e1fd6323ba0375883
oskar <oskarsinger@gmail.com>
2018-09-26 13:07:11 -0500

    Tree nodes store values

--- drrobert/data_structures/suffix_tree.py
+++ drrobert/data_structures/suffix_tree.py
@@ -1,61 +1,72 @@
 class SuffixTree:
 
-    def __init__(self, vocabulary):
-        self.vocabulary = vocabulary
+    def __init__(self, vocabulary=None):
+
         self.tree = {}
 
-        self._populate(self.vocabulary)
+        if vocabulary is not None:
+            self._populate(vocabulary)
 
     def _populate(self, vocabulary):
 
         for word in vocabulary:
-            self._insert(word)
+            self.insert(word)
 
-    def _insert(self, word):
+    def insert(self, word, value=None):
 
-        self._recursive_insert(self.tree, word)
+        self._recursive_insert(self.tree, word, value)
         
-    def _recursive_insert(self, subtree, subword):
+    def _recursive_insert(self, subtree, subword, value):
+
         letter = subword[0]
         is_terminal = len(subword) == 1
 
         if letter in subtree:
             if is_terminal:
                 subtree[letter].is_terminal = True
+                subtree[letter].value = value
             else:
                 self._recursive_insert(
                     subtree[letter].children, 
-                    subword[1:])
+                    subword[1:],
+                    value)
         else:
-            subtree[letter] = Node(is_terminal)
+            subtree[letter] = Node(
+                is_terminal, 
+                value=value)
 
             if not is_terminal:
                 self._recursive_insert(
                     subtree[letter].children, 
-                    subword[1:])
+                    subword[1:],
+                    value)
 
-    def has_word(self, word):
+    def has(self, word):
         
         return self._recursive_has_word(self.tree, word)
     
     def _recursive_has_word(self, subtree, subword):
+
         letter = subword[0]
+        has_word = False
+        value = None
 
         if letter in subtree:
             if len(subword) == 1:
                 if subtree[letter].is_terminal:
-                    return True
-                else:
-                    return False
+                    has_word = True
+                    value = subtree[letter].value
             else:
-                return self._recursive_has_word(
+                (has_word, value) = self._recursive_has_word(
                     subtree[letter].children,
                     subword[1:])
-        else:
-            return False
 
-class Node():
+        return (has_word, value)
 
-    def __init__(self, is_terminal):
+class Node:
+
+    def __init__(self, is_terminal, value=None):
+
         self.is_terminal = is_terminal
         self.children = {}
+        self.value = value
